91403c036462ad617c415f4f9d50d988db9b6643
rbrennan3 <rbrenna6@vols.utk.edu>
2022-03-08 11:31:51 -0500

    Created drop down
    Can now pick stock based on budget

--- Server.R
+++ Server.R
@@ -1,6 +1,24 @@
-function(input, output) {
+function(input, output, session) {
   
   # You can access the values of the second widget with input$slider2, e.g.
   output$range <- renderPrint({ input$slider1 })
+  stock_range <- reactive({
+    cbind(input$slider1[1], input$slider1[2])
+    })
+  
+  output$SliderText <- renderText(stock_range())
+  
+  output$dropdown <- renderUI({
+    budget_stock <- stocks$symbol[stocks$close >= input$slider1[1] & stocks$close <= input$slider1[2]]
+    
+    selectInput(
+    inputId = "stock_selected",
+    label = "Select a Stock:",
+    choices = unique(budget_stock)
+    )
+  })
+ 
+  
+
   
 }
--- UI.R
+++ UI.R
@@ -6,7 +6,12 @@ fluidPage(
     column(4,
            
            sliderInput("slider1", label = h3("Available Budget"), min = floor(min(stocks$close)), 
-                       max = ceiling(max(stocks$close)), value = c(200, 1000))
+                       max = ceiling(max(stocks$close)), value = c(200, 1000)),
+           
+           textOutput("SliderText"),
+           uiOutput("dropdown")
     )
+    
   )
 )
+
